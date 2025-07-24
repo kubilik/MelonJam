d040fffba8d14488390ad094650f760f87a66819
Kubilay Teker <yusuf-kubi@hotmail.com>
2025-07-25 01:35:01 +0300

    elimize coconot geliyo

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -3,17 +3,35 @@ using UnityEngine;
 public class PlayerInventory : MonoBehaviour
 {
     public bool hasOrder = false;
+    public GameObject orderVisualPrefab;
+    private GameObject orderInstance;
+
+    public Transform handTransform;
 
     public void PickUpOrder()
     {
+        if (hasOrder) return;
+
         hasOrder = true;
         Debug.Log("Order picked up.");
+
+        if (orderVisualPrefab != null && handTransform != null)
+        {
+            orderInstance = Instantiate(orderVisualPrefab, handTransform.position, handTransform.rotation, handTransform);
+        }
     }
 
     public void DeliverOrder()
     {
+        if (!hasOrder) return;
+
         hasOrder = false;
         Debug.Log("Order delivered.");
+
+        if (orderInstance != null)
+        {
+            Destroy(orderInstance);
+        }
     }
 
     public bool IsHoldingOrder()
